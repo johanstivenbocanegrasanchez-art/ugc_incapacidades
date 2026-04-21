31f5b579b779170e9da97097a571f16448361f41
Nicol <rnicolcaterine042007@gmail.com>
2026-04-21 15:25:50 -0500

    estilo

--- views/shared/detalle.php
+++ views/shared/detalle.php
@@ -24,7 +24,7 @@ $baseUrl = Config::baseUrl();
     <?php if (!empty($solicitud['RUTA_COMPROBANTE'])): ?>
     <div class="detail-row"><dt>Documento adjunto</dt>
       <dd>
-        <div class="archivo-adjunto" style="margin:0;max-width:400px;">
+        <div class="archivo-adjunto" style="margin:0;max-width:600px;">
           <span class="archivo-icono"><svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><polyline points="9 15 12 12 15 15"></polyline></svg></span>
           <div class="archivo-info">
             <div class="archivo-nombre"><?= htmlspecialchars(basename($solicitud['RUTA_COMPROBANTE'])) ?></div>
